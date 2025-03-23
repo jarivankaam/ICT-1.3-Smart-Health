e82f8eba99f550d2c006041306ce2ba2de113817
mikeyboy52 <mikevanlommel2007@gmail.com>
2025-03-23 16:58:22 +0100

    Added Menu to all pages

--- Assets/Scripts/MenuAnimation.cs
+++ Assets/Scripts/MenuAnimation.cs
@@ -1,13 +1,18 @@
 using DG.Tweening;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class MenuAnimation : MonoBehaviour
 {
+
     public GameObject menuButton;
     public Transform AnimateMenu;
     Vector2 menuOnScreen = new Vector2((float)719.5, (float)0.49);
     Vector2 menuOffScreen = new Vector2(1203, (float)0.49);
 
+    public void Start()
+    {
+    }
     public void MenuAppear()
     {
         AnimateMenu.DOLocalMove(menuOnScreen, 1);
@@ -19,18 +24,4 @@ public class MenuAnimation : MonoBehaviour
         AnimateMenu.DOLocalMove(menuOffScreen, 1);
         menuButton.SetActive(true);
     }
-
-
-    //// Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
-
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-
 }
