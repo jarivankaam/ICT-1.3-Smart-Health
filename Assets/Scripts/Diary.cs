26df13287bc1719e421f437588d85b97a7d86f04
BlaStrijder <Blastrijder@gmail.com>
2025-04-01 12:31:41 +0200

    DiaryConnectionDatabaseBegin

--- Assets/Scripts/Diary.cs
+++ Assets/Scripts/Diary.cs
@@ -1,24 +1,51 @@
 using TMPro;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class Diary : MonoBehaviour
 {
     public TMP_InputField diaryInput;
-    private static string savedDiaryText = "";
+    private static string SavedDiaryText = "";
 
     void Start()
     {
-        diaryInput.text = savedDiaryText;
+        diaryInput.text = SavedDiaryText;
+        //MakeDiary();
     }
 
     public void DeleteDiary()
     {
-        savedDiaryText = "";
-        diaryInput.text = savedDiaryText;
+        SavedDiaryText = "";
+        diaryInput.text = SavedDiaryText;
     }
 
     public void SaveDiary()
     {
-        savedDiaryText = diaryInput.text;
+        SavedDiaryText = diaryInput.text;
     }
+
+
+    //public async task SaveDiaryToDatabase(string savedDiaryText)
+    //{
+        //savedDiaryText = SavedDiaryText;
+        //var request = new PostRegisterRequestDto()
+        //{
+        //    userId = userId,
+        //    savedDiaryText = savedDiaryText,
+        //};
+        //var jsondata = JsonUtility.ToJson(request);
+        //var response = await PerformApiCall("https://localhost:7032/api/Dairy/:id", "PUT", jsondata);
+    //}
+
+    //public async Task MakeDiary() //maakt diary 
+    //{
+    //    //string MakeDiaryText = "";
+    //    //var request = new PostRegisterRequestDto()
+    //    //{
+    //    //    userId = userId,
+    //    //    MakeDiaryText = MakeDiaryText,
+    //    //};
+    //    //var jsondata = JsonUtility.ToJson(request);
+    //    //var response = await PerformApiCall("https://localhost:7032/api/Dairy", "POST", jsondata);
+    //}
 }
