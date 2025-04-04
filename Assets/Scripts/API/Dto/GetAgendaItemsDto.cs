df51cc5e23da91904458a8ca86b54bb237325cf3
Az3rAp1 <jjj.vankaam@student.avans.nl>
2025-04-04 11:38:31 +0200

    Update changes in ShowingAgendaItems branch

---
+++ Assets/Scripts/API/Dto/GetAgendaItemsDto.cs
@@ -0,0 +1,13 @@
+﻿public class GetAgendaItemsDto
+{
+    public string id;
+    public string content;
+    public bool video;
+    public string videoPath;
+    public bool medicine;
+    public string toolTipContent;
+    public string imagePath;
+    public string position;
+    public string type;
+    public string timeLineID;
+}
